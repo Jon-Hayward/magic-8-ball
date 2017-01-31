54c359c3eed6c26edeac7c7f40e3ed4e328da773
Jon-Hayward <jonhayward88@gmail.com>
2017-01-31 11:14:09 +0000

    Add files via upload

---
+++ cache/8 balla.py
@@ -0,0 +1,123 @@
+# Import the modules
+import os
+os.system ("COLOR 04")
+
+import sys
+import random
+
+
+print " 	         _......._"
+print "       .-:::::::::::-."
+print "     .:::::::::::::::::."
+print " 	   :::::::' .-. `:::::::"
+print "   :::::::  :   :  :::::::"
+print " 		 ::::::::  :   :  ::::::::"
+print "  :::::::::._`-'_.:::::::::"
+print " 	 :::::::::' .-. `:::::::::"
+print "  ::::::::  :   :  ::::::::"
+print "   :::::::  :   :  :::::::"
+print "		    :::::::._`-'_.:::::::"
+print "     `:::::::::::::::::'"
+print "   	    `-:::::::::::-'"
+print "          `'''''''`"
+print "DON'T DO THAT AGAIN"
+
+ans = True
+
+while ans:
+    question = raw_input("Ask the magic 8 ball a question: (press enter to quit) ")
+    
+    answers = random.randint(1,8)
+
+    if question == "who are you?":
+		print " 		                    ,-."
+		print "       ___,---.__          /'|`\          __,---,___"
+		print "    ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-."
+		print "  ,'        |           ~'\     /`~           |        `."
+		print " [      ___                `. ,'          ,  ,  ___      ]"
+		print "|    ,-'   `-.__   _         |        ,    __,-'   `-.    |"
+		print "|   /          /\_  `   .    |    ,      _/\          \   |"
+		print "\  |           \ \`-.___ \   |   / ___,-'/ /           |  /"
+		print " \  \           | `._   `\\  |  //'   _,' |           /  /"
+		print "  `-.\         /'  _ `---'' , . ``---' _  `\         /,-'"
+		print "     ``       /     \    ,='/ \`=.    /     \       ''"
+		print "             |__   /|\_,--.,-.--,--._/|\   __|"
+		print "             /   ./  \\ \ |  |  | /,//  \,   ]"
+		print "            /   /     ||-\ -- -- -/-|     \   ]"
+		print "           |   |      ||-\ -- -- -/-|     |   |"
+		print "            \   \__, /'\_\_\ | /_/_/`\ .__/   / "
+		print "             `-._,-' \_     `~'     _/ `-._,-'"
+		print "                       `-._______,-'   "
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+		os.system ("COLOR 04")
+		os.system ("COLOR 40")
+	
+		os.system('cls')
+		execfile("C:\\Users\\jhayward\\Desktop\\LEARNING PYTHON\\magic 8 ball\\cache\\cross ball.py")
+		
+		
+    if question == "":
+        sys.exit()
+    
+
+	
+    elif answers == 1:
+        print "Yeah"
+    
+    elif answers == 2:
+        print "Probably"
+    
+    elif answers == 3:
+        print "I guess so"
+    
+    elif answers == 4:
+        print "I'm tired ask again later"
+    
+    elif answers == 5:
+        print "Focus your dome piece and ask again"
+    
+    elif answers == 6:
+        print "Can't hear you, try again"
+    
+    elif answers == 7:
+        print "My reply is hmm... probs not"
+    
+    elif answers == 8:
+        print "My sources say no dizzle"
---
+++ cache/cross ball.py
@@ -0,0 +1,145 @@
+# Import the modules
+import os
+os.system ("COLOR 0c")
+
+import sys
+import random
+
+
+print "          _......._"
+print "       .-:::::::::::-."
+print "     .:::::::::::::::::."
+print "    :::::::::.-.:::::::::"
+print "   :::::::::::  ::::::::::"
+print "  ::::::::::::  :::::::::::"
+print "  ::::::::::::  :::::::::::"
+print "  ::::::::::::  :::::::::::"
+print "  ::::::::          :::::::"
+print "   :::::::::::  ::::::::::"
+print "    :::::::::.__.::::::::"
+print "     `:::::::::::::::::'"
+print "       `-:::::::::::-'"
+print "          `'''''''`"
+
+ans = True
+
+while ans:
+    question = raw_input("Ask the magic 8 ball a question: (press enter to quit) ")
+    
+    answers = random.randint(1,8)
+
+    if question == "who are you?":
+		print " 		                    ,-."
+		print "       ___,---.__          /'|`\          __,---,___"
+		print "    ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-."
+		print "  ,'        |           ~'\     /`~           |        `."
+		print " [      ___                `. ,'          ,  ,  ___      ]"
+		print "|    ,-'   `-.__   _         |        ,    __,-'   `-.    |"
+		print "|   /          /\_  `   .    |    ,      _/\          \   |"
+		print "\  |           \ \`-.___ \   |   / ___,-'/ /           |  /"
+		print " \  \           | `._   `\\  |  //'   _,' |           /  /"
+		print "  `-.\         /'  _ `---'' , . ``---' _  `\         /,-'"
+		print "     ``       /     \    ,='/ \`=.    /     \       ''"
+		print "             |__   /|\_,--.,-.--,--._/|\   __|"
+		print "             /   ./  \\ \ |  |  | /,//  \,   ]"
+		print "            /   /     ||--+--|--+-/-|     \   ]"
+		print "           |   |     /'\_\_\ | /_/_/`\     |   |"
+		print "            \   \__, \_     `~'     _/ .__/   /"
+		print "             `-._,-'   `-._______,-'   `-._,-'"
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR 0F")
+		os.system ("COLOR f0")
+		os.system ("COLOR f4")
+		os.system ("COLOR 04")
+		os.system ("COLOR f4")
+		os.system ("COLOR 04")
+		os.system ("COLOR f4")
+		os.system ("COLOR 04")
+		os.system ("COLOR fc")
+		os.system ("COLOR 0c")
+		os.system ("COLOR fc")
+		os.system ("COLOR 0c")
+		os.system ("COLOR fc")
+		os.system ("COLOR 0c")
+		os.system ("COLOR fc")
+		os.system ("COLOR f0")
+	
+		os.system('cls')
+		execfile("C:\\Users\\jhayward\\Desktop\\LEARNING PYTHON\\8 ball.py")
+		
+		
+    if question == "":
+        sys.exit()
+    
+
+	
+    elif answers == 1:
+        print "You're fucked!"
+    
+    elif answers == 2:
+        print "You're fucked!"
+    
+    elif answers == 3:
+        print "You're fucked!"
+    
+    elif answers == 4:
+        print "You're fucked!"
+    
+    elif answers == 5:
+        print "You're fucked!"
+    
+    elif answers == 6:
+        print "You're fucked!"
+    
+    elif answers == 7:
+        print "You're fucked!"
+    
+    elif answers == 8:
+        print "You're fucked!"
+
+
+		
+		
